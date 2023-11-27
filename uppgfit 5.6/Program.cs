bdf674eb0bd2782f5aba5c3285405b59df0a902a
en301 <emilsjostrand1000@gmail.com>
2023-11-27 11:52:29 +0100

    Add project files.

---
+++ uppgfit 5.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int produkt = 1;
+            Console.WriteLine("Skriv ut talen du vill multiplicera med med ett * tecken mellan");
+            string svar = Console.ReadLine();
+            string[] taltext = svar.Split('*');
+            for (int i  = 0; i < taltext.Length; i++)
+            {
+                produkt *= int.Parse(taltext[i]);
+
+            }
+            Console.WriteLine(produkt);
+        }
+    }
+}
